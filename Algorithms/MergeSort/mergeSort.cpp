fd4a4a684384f1b45ffb72cc47b52c601f40c293
MarianBirindzhiev <marianbirindzhiev@gmail.com>
2022-11-21 17:10:39 +0200

    uploading mergeSort

--- Algorithms/MergeSort/mergeSort.cpp
+++ Algorithms/MergeSort/mergeSort.cpp
@@ -3,51 +3,51 @@
 #include <iostream>
 
 template <typename T>
-void merge( std::vector<T> array, const int leftIndex,const int midIndex ,const int rightIndex)
+void merge(std::vector<T> array, const int leftIndex, const int midIndex, const int rightIndex)
 {
     const int leftArrayCapacity = midIndex - leftIndex;
     const int rightArrayCapacity = rightIndex - midIndex + 1;
-    
-    int indexOfLeftArray=0;
-    int indexOfRightArray=0;
-    int indexOfMergedArray=leftIndex;
 
-    T* leftArray= new T[leftArrayCapacity];
-    T* rightArray= new T[rightArrayCapacity];
+    int indexOfLeftArray = 0;
+    int indexOfRightArray = 0;
+    int indexOfMergedArray = leftIndex;
+
+    T* leftArray = new T[leftArrayCapacity];
+    T* rightArray = new T[rightArrayCapacity];
 
     for (size_t currentIndex = 0; currentIndex < leftArrayCapacity; currentIndex++)
     {
-        leftArray[currentIndex]=array[currentIndex];
+        leftArray[currentIndex] = array[currentIndex];
     }
     for (size_t currentIndex = 0; currentIndex < rightArrayCapacity; currentIndex++)
     {
-        rightArray[currentIndex]=array[currentIndex];
+        rightArray[currentIndex] = array[currentIndex];
     }
 
-    while (indexOfLeftArray<leftArrayCapacity && indexOfRightArray < rightArrayCapacity)
+    while (indexOfLeftArray < leftArrayCapacity && indexOfRightArray < rightArrayCapacity)
     {
         T currentLeftArrayElement = leftArray[indexOfLeftArray];
         T currentRightArrayElement = rightArray[indexOfRightArray];
-        if (currentLeftArrayElement<=currentRightArrayElement)
+        if (currentLeftArrayElement <= currentRightArrayElement)
         {
-            array[indexOfMergedArray]=currentLeftArrayElement;
+            array[indexOfMergedArray] = currentLeftArrayElement;
             ++indexOfLeftArray;
         }
         else
         {
-            array[indexOfMergedArray]=currentRightArrayElement;
+            array[indexOfMergedArray] = currentRightArrayElement;
             ++indexOfRightArray;
         }
         ++indexOfMergedArray;
     }
 
-    while (indexOfLeftArray<leftArrayCapacity)
+    while (indexOfLeftArray < leftArrayCapacity)
     {
         array[indexOfMergedArray] = leftArray[indexOfLeftArray];
         ++indexOfLeftArray;
         ++indexOfMergedArray;
     }
-        while (indexOfRightArray<rightArrayCapacity)
+    while (indexOfRightArray < rightArrayCapacity)
     {
         array[indexOfMergedArray] = rightArray[rightIndex];
         ++indexOfRightArray;
@@ -56,30 +56,30 @@ void merge( std::vector<T> array, const int leftIndex,const int midIndex ,const 
 
     delete[] leftArray;
     delete[] rightArray;
-    
+
 }
 
 template <typename T>
-void mergeSort(std::vector<T> inputArray,int begin, int end)
+void mergeSort(std::vector<T> inputArray, int begin, int end)
 {
-    if(begin>=end)
+    if (begin >= end)
     {
         return;
     }
-    int midIndex=begin + (end-begin)/2;
-    mergeSort(inputArray,begin,midIndex);
-    mergeSort(inputArray,midIndex+1,end);
-    merge(inputArray,begin,midIndex,end);
+    int midIndex = begin + (end - begin) / 2;
+    mergeSort(inputArray, begin, midIndex);
+    mergeSort(inputArray, midIndex + 1, end);
+    merge(inputArray, begin, midIndex, end);
 }
 
 
 int main()
 {
-    std::vector<int> A{141,2,24,5,5,61,122,667};
-    mergeSort(A, 0, A.size()-1);
-    for (size_t i = 0; i < A.size()-1; i++)
+    std::vector<int> A{ 141,2,24,5,5,61,122,667 };
+    mergeSort(A, 0, A.size() - 1);
+    for (size_t i = 0; i < A.size() - 1; i++)
     {
-        std::cout<<A[i]<<'\n';
+        std::cout << A[i] << '\n';
     }
-    
+
 }
