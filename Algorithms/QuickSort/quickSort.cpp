6c744d0b81d78b6900077153e99b5e3300828518
MarianBirindzhiev <marianbirindzhiev@gmail.com>
2022-11-21 17:20:09 +0200

    pushing quicksort

--- Algorithms/QuickSort/quickSort.cpp
+++ Algorithms/QuickSort/quickSort.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+
+
+template<typename T>
+int partition(T* passedArray, int low, int high)
+{
+	int pivot = passedArray[high];
+	int smallerElementIndex = low - 1;
+	for (size_t j = low; j <= high-1; j++)
+	{
+		if (passedArray[j]<pivot)
+		{
+			smallerElementIndex++;
+			std::swap(passedArray[smallerElementIndex], passedArray[j]);
+		}
+	}
+	std::swap(passedArray[smallerElementIndex + 1], passedArray[high]);
+	return (smallerElementIndex + 1);
+}
+
+template<typename T>
+void quickSort(T* passedArray, int low, int high)
+{
+	if (low < high)
+	{
+		int partitioningIndex = partition(passedArray, low, high);
+		quickSort(passedArray, low, partitioningIndex - 1);
+		quickSort(passedArray, partitioningIndex + 1, high);
+	}
+}
 
 int main()
 {
-    
+
 }
 
 
